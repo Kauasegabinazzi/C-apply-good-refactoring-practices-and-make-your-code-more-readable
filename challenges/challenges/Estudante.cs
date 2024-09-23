bba472e1c462c91cadc1baf14da16a30b4f5fc8d
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-09-23 10:07:25 -0300

    3-commit

---
+++ challenges/challenges/Estudante.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+namespace challenges;
+
+public class Estudante
+{
+
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+    public string Matricula { get; set; }
+
+    public void ImprimirDados(string nome, int idade, string matricula)
+    {
+        Console.WriteLine("Nome: " + nome);
+        Console.WriteLine("Idade: " + (idade));
+        Console.WriteLine("Matricula: " + (matricula));
+    }
+
+    internal void Show()
+    {
+
+        var joao = new Estudante();
+        List<Estudante> args = new List<Estudante>();
+
+        joao.ImprimirDados(nome: args[0].Nome, idade: args[1].Idade, matricula: args[2].Matricula);
+    }
+
+}
+
