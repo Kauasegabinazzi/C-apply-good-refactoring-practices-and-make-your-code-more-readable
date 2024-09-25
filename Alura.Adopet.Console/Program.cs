5ff139ff95a82c7b28f39e2c17f006a6a26cac4c
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-09-24 21:55:27 -0300

    5-commit

--- Alura.Adopet.Console/Program.cs
+++ Alura.Adopet.Console/Program.cs
@@ -19,39 +19,9 @@ try
             await import.ImportFilePetAsync(path: args[1]);
             break;
         case "help":
-            Console.WriteLine("Lista de comandos.");
-            // se não passou mais nenhum argumento mostra help de todos os comandos
-            if (args.Length == 1)
-            {
-                Console.WriteLine("adopet help <parametro> ous simplemente adopet help  " +
-                     "comando que exibe informações de ajuda dos comandos.");
-                Console.WriteLine("Adopet (1.0) - Aplicativo de linha de comando (CLI).");
-                Console.WriteLine("Realiza a importação em lote de um arquivos de pets.");
-                Console.WriteLine("Comando possíveis: ");
-                Console.WriteLine($" adopet import <arquivo> comando que realiza a importação do arquivo de pets.");
-                Console.WriteLine($" adopet show   <arquivo> comando que exibe no terminal o conteúdo do arquivo importado." + "\n\n\n\n");
-                Console.WriteLine("Execute 'adopet.exe help [comando]' para obter mais informações sobre um comando." + "\n\n\n");
-            }
-            // exibe o help daquele comando específico
-            else if (args.Length == 2)
-            {
-                string showCommand = args[1];
-                if (showCommand.Equals("import"))
-                {
-                    Console.WriteLine(" adopet import <arquivo> " +
-                        "comando que realiza a importação do arquivo de pets.");
-                }
-                if (showCommand.Equals("show"))
-                {
-                    Console.WriteLine(" adopet show <arquivo>  comando que " +
-                        "exibe no terminal o conteúdo do arquivo importado.");
-                }
-                if (showCommand.Equals("list"))
-                {
-                    Console.WriteLine(" adopet list <arquivo>  comando que " +
-                        "exibe no terminal o conteúdo do arquivo importado.");
-                }
-            }
+            var help = new help();
+            help.ShowHelpList(path: args[1], args: args);
+
             break;
         case "show":
             // args[1] é o caminho do arquivo a ser exibido
---
+++ Alura.Adopet.Console/help.cs
@@ -0,0 +1,42 @@
+﻿namespace Alura.Adopet.Console;
+
+internal class help
+{
+    public void ShowHelpList(string path, string[] args)
+    {
+        System.Console.WriteLine("Lista de comandos.");
+        // se não passou mais nenhum argumento mostra help de todos os comandos
+        if (path.Length == 1)
+        {
+            System.Console.WriteLine("adopet help <parametro> ous simplemente adopet help  " +
+                 "comando que exibe informações de ajuda dos comandos.");
+            System.Console.WriteLine("Adopet (1.0) - Aplicativo de linha de comando (CLI).");
+            System.Console.WriteLine("Realiza a importação em lote de um arquivos de pets.");
+            System.Console.WriteLine("Comando possíveis: ");
+            System.Console.WriteLine($" adopet import <arquivo> comando que realiza a importação do arquivo de pets.");
+            System.Console.WriteLine($" adopet show   <arquivo> comando que exibe no terminal o conteúdo do arquivo importado." + "\n\n\n\n");
+            System.Console.WriteLine("Execute 'adopet.exe help [comando]' para obter mais informações sobre um comando." + "\n\n\n");
+        }
+        // exibe o help daquele comando específico
+        else if (path.Length == 2)
+        {
+            string showCommand = args[1];
+            if (showCommand.Equals("import"))
+            {
+                System.Console.WriteLine(" adopet import <arquivo> " +
+                    "comando que realiza a importação do arquivo de pets.");
+            }
+            if (showCommand.Equals("show"))
+            {
+                System.Console.WriteLine(" adopet show <arquivo>  comando que " +
+                    "exibe no terminal o conteúdo do arquivo importado.");
+            }
+            if (showCommand.Equals("list"))
+            {
+                System.Console.WriteLine(" adopet list <arquivo>  comando que " +
+                    "exibe no terminal o conteúdo do arquivo importado.");
+            }
+        }
+    }
+}
+
