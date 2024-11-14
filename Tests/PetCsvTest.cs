9076d169e9c2cbfc5952342f2fcb356bd58edc86
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-11-14 20:02:12 -0300

    18-commit

---
+++ Tests/PetCsvTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class PetCsvTest
+    {
+        [Fact]
+        public void ReturnPet()
+        {
+            //Arrange
+            //Act
+            //Asset
+        }
+
+    }
+}
