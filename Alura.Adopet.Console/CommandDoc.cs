fabc62c4c38907c0dc13373261fcf75ccc0e4624
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-12-10 21:14:06 -0300

    25-commit

--- Alura.Adopet.Console/CommandDoc.cs
+++ Alura.Adopet.Console/CommandDoc.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace Alura.Adopet.Console
 {
     [AttributeUsage(AttributeTargets.Class)]
-    internal class CommandDoc : System.Attribute
+    public class CommandDoc : System.Attribute
     {
         public CommandDoc(string instruction, string documentation)
         {
---
+++ Tests/GeraDocument.cs
@@ -0,0 +1,28 @@
+﻿using Alura.Adopet.Console;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class GeraDocument
+    {
+        [Fact]
+        public void WhenExistCommand()
+        {
+
+            Assembly assemblyTypeDoc = Assembly.
+                GetAssembly(typeof(CommandDoc))!;
+
+            Dictionary<string, CommandDoc> dicionario =
+                DocumentationSystem.toDictionary(assemblyTypeDoc);
+
+            NUnit.Framework.Legacy.ClassicAssert.IsNotEmpty(dicionario);
+        }
+    }
+}
